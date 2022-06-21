d692bd7f7e3032ecfc58d085acf4291ed0208e7e
Hunter Saunders <hjvs98@gmail.com>
2022-06-21 12:35:01 -0600

    set up app.get for styles

--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ app.use(express.json());
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+app.get("/styles", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.css"));
+});
 
 const port = process.env.PORT || 3005;
 
